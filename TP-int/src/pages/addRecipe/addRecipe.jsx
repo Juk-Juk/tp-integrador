5d5e316fb07b69f530dd16414148a16cad2d5ff1
Juk <Jukzhee@gmail.com>
2025-01-05 17:55:34 -0300

    File case problem

--- TP-int/src/pages/addRecipe/addRecipe.jsx
+++ TP-int/src/pages/addRecipe/addRecipe.jsx
@@ -5,7 +5,7 @@ import { useGetAuthors } from "../../hooks/authors/useGetAuthors";
 import { toast } from "react-toastify";
 
 import "react-datepicker/dist/react-datepicker.css";
-import './AddRecipe.css';
+import './addRecipe.css';
 
 const initRecipeValue = {
     author: '',
